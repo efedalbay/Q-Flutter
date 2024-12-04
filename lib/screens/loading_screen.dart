ed64ae728d2b89d8b6f24922cebe5e8223a331f6
Keyvan Arasteh <keyvan.arasteh@gmail.com>
2024-12-04 23:54:04 +0300

    lets save progress
    baya sey yaptim :-D
    
    - ekranlari ekledim
    - arka plan renki tanimladim
    - kendime ozel Alt menu widget yaptim tum ekranlarda kullandim :-O
    ....

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,11 @@
 // acilis ekranimiz buraya gelicek
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+
+import '../core/constants.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -8,48 +13,46 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 35, 47, 59),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Logo ve yükleme göstergesi bölümü
-          Container(
-            width: double.infinity,
-            child: Column(
-              children: [
-                // Logo bölümü
-                Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.asset(
-                    'assets/images/logo.webp',
-                    fit: BoxFit.contain,
-                  ),
+      backgroundColor: arkaplanRenkim, // .fromARGB(255, 35, 47, 59),
+      body: SizedBox.expand(
+        // width: double.infinity,
+        child: Column(
+          children: [
+            // Logo bölümü
+            Expanded(
+              child: Container(
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  'assets/images/logo.webp',
+                  fit: BoxFit.contain,
                 ),
-
-                const SizedBox(height: 30),
-
-                // Yükleme ikonu
-                const Icon(
-                  CupertinoIcons.arrow_2_circlepath,
-                  size: 40,
-                  color: Colors.blue,
-                ),
-
-                const SizedBox(height: 20),
-
-                // Yükleniyor yazısı
-                const Text(
-                  'Yükleniyor...',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+
+            // Yükleniyor yazısı
+            // CircularProgressIndicator(),
+
+            InkWell(
+              onTap: () => context.go("/home"),
+              child: SizedBox(
+                width: 300,
+                child: DotLottieLoader.fromAsset(
+                  "assets/motions/loading.lottie",
+                  frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                    if (dotlottie != null) {
+                      return Lottie.memory(dotlottie.animations.values.single);
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ),
+            ),
+            // biraz boslok lazim
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
